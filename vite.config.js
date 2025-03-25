1002774fc4f01b531bf0ad00f27ffff4cc3498af
kharlaienko <55911912+kharlaienko@users.noreply.github.com>
2025-03-25 21:06:24 +0200

    add deploy config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import svgLoader from 'vite-svg-loader'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/calendar-test-app/'
+    : '/'
 })
